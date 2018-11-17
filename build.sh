f28abe820991354b010c442d40782b16eb7945bc
yasu-hide <5038337+yasu-hide@users.noreply.github.com>
2018-11-17 22:01:58 +0900

    Output log to stdout

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-DOCKER_LOG=/var/log/docker.log
-DEBUG_LOG=/dev/null
-
 echo ">> [$(date)] Build for TARGET_DEVICE $*"
 
-if [ "$DEBUG" = true ]; then
-    DEBUG_LOG=$DOCKER_LOG
-fi
-
 if [ "$USE_CCACHE" = 1 ]; then
-    ccache -M $CCACHE_SIZE 2>&1 >&$DEBUG_LOG
+    ccache -M $CCACHE_SIZE 2>&1
 fi
 
 git config --global user.name $GIT_USER_NAME
@@ -18,30 +11,30 @@ git config --global user.email $GIT_USER_MAIL
 cd $SRC_DIR
 
 if ! [ -f "$SRC_DIR/.repo/manifest.xml" ]; then
-    echo ">> [$(date)] Initializing repository" >> $DOCKER_LOG
-    yes | repo init -u git://github.com/lineageos/android.git -b $BRANCH_NAME 2>&1 >&$DEBUG_LOG
+    echo ">> [$(date)] Initializing repository"
+    yes | repo init -u git://github.com/lineageos/android.git -b $BRANCH_NAME 2>&1
 fi
 
-echo ">> [$(date)] Syncing repository" >> $DOCKER_LOG
-repo sync -j16 -f 2>&1 >&$DEBUG_LOG
+echo ">> [$(date)] Syncing repository"
+repo sync -j16 -f 2>&1
 
-echo ">> [$(date)] Preparing build environment" >> $DOCKER_LOG
-source build/envsetup.sh 2>&1 >&$DEBUG_LOG
+echo ">> [$(date)] Preparing build environment"
+source build/envsetup.sh 2>&1
 source <( curl https://gist.githubusercontent.com/yasu-hide/f3f160b7a4569ee3940420bd5613523d/raw/repopick.sh )
 
 for codename in $*; do
 
-    echo ">> [$(date)] Starting build for $codename" >> $DOCKER_LOG
-    if brunch $codename 2>&1 >&$DEBUG_LOG; then
-        echo ">> [$(date)] Moving build artifacts for $codename to '$OUT_DIR'" >> $DOCKER_LOG
+    echo ">> [$(date)] Starting build for $codename"
+    if brunch $codename 2>&1; then
+        echo ">> [$(date)] Moving build artifacts for $codename to '$OUT_DIR'"
         cd $SRC_DIR
-        find out/target/product/$codename -name '*UNOFFICIAL*.zip*' -exec mv {} $OUT_DIR \; >&$DEBUG_LOG
+        find out/target/product/$codename -name '*UNOFFICIAL*.zip*' -exec mv {} $OUT_DIR \;
     else
-        echo ">> [$(date)] Failed build for $codename" >> $DOCKER_LOG
+        echo ">> [$(date)] Failed build for $codename"
     fi
     
-    echo ">> [$(date)] Cleaning build for $codename" >> $DOCKER_LOG
-    mka clean 2>&1 >&$DEBUG_LOG
+    echo ">> [$(date)] Cleaning build for $codename"
+    mka clean 2>&1
     
-    echo ">> [$(date)] Finishing build for $codename" >> $DOCKER_LOG
+    echo ">> [$(date)] Finishing build for $codename"
 done
