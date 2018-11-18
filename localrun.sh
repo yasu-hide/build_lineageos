1ad5b4d62ccf54760fd7e3ff240b02948fabfa37
yasu-hide <5038337+yasu-hide@users.noreply.github.com>
2018-11-18 20:35:12 +0900

    Change the docker image name

--- localrun.sh
+++ localrun.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 BRANCH_NAME=lineage-16.0
 docker run \
-    -e BRANCH_NAME=$BRANCH_NAME \
+    -e BRANCH_NAME="$BRANCH_NAME" \
+    -e DISTCC_HOSTS="$DISTCC_HOSTS" \
     -v /build/ccache:/lineage/ccache \
     -v /build/src1:/lineage/src \
     -v /build/out:/lineage/out \
     -v $(pwd)/local_manifests:/lineage/src/.repo/local_manifests \
     --ulimit nproc=32768  --ulimit nofile=1048576 \
     --user root \
-    -e DEBUG=$DEBUG \
-    docker.io/yasu-hide/build_lineageos $*
+    vet5lqplpecmpnqb/docker-build-lineageos $*
